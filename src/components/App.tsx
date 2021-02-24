70aba94d48e433e9fa822f7e875e1998420f187c
jthanawat <kla13thanawat@gmail.com>
2021-02-24 15:22:39 +0700

    consume store state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
-import { useState } from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../state';
+import RepositoriesList from './RepositoriesList';
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search For a Package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
 };
 
 export default App;
